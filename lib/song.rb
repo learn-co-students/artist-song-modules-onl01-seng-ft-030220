e60b78f6b5e37c88a8ef1a7f7960e20b4700402b
marsha <marshaannon@gmail.com>
2020-03-20 21:45:06 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -7,26 +7,18 @@ class Song
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
   
-    attr_accessor :name
-    attr_reader :artist
+  attr_accessor :name
+  attr_reader :artist
 
-    @@songs = []
+  @@songs = []
 
   def initialize
     self.class.all << self
   end
 
-  #def self.find_by_name(name)
-   # @@songs.detect{|a| a.name == name}
-  #end
-
-  #def self.all
-   #@@songs
-  #end
-
-  #def self.reset_all
-   # self.all.clear
-  #end
+  def self.all
+   @@songs
+  end
 
   def self.count
     self.all.count
@@ -36,7 +28,4 @@ class Song
     @artist = artist
   end
 
-  #def to_param
-    #name.downcase.gsub(' ', '-')
-  #end
 end
